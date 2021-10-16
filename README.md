e609c46ecc511775d876510e9598e90e20254b43
unknown <ocardenasmartinez@gmail.com>
2021-10-16 20:26:34 -0300

    add business tests

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Products Service
 ## Installation
 In the console
+
+```bash
 1.- git clone https://github.com/ocardenasmartinez/seidor-products.git
+2.- 
+```
+
--- src/test/java/cl/falabella/configurators/ProductsConfigurationTest.java
+++ src/test/java/cl/falabella/configurators/ProductsConfigurationTest.java
@@ -2,8 +2,11 @@ package cl.falabella.configurators;
 
 import cl.falabella.buisness.ProductBusiness;
 import cl.falabella.business.mocks.ProductBusinessMock;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 public class ProductsConfigurationTest {
